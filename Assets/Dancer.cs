92ddcb3dc08b0d2aa9e04f9329d14b06a4665ac3
pijussJ <pijus.jarusevicius3@gmail.com>
2024-03-21 17:45:13 +0200

    Dancer

---
+++ Assets/Dancer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dancer : MonoBehaviour
+{
+    [Range(0, 5)]public float rotateSpeed;
+    public ParticleSystem boom;
+    private void Start()
+    {
+        Analyzer.onVolumeChanged.AddListener(Dance);
+    }
+    public void Dance(float volume)
+    {
+        transform.localScale = new Vector3(volume * 3f, volume * 3f, volume * 3f);
+        if (volume >= 0.3f)
+        {
+            boom.Play();
+        }
+        //transform.Rotate(0, 0, volume * rotateSpeed);
+        //transform.position = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(3, 2, 0), volume * 5);
+    }
+}
